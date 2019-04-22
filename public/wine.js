89c431dea53fad63f3183f1a6202b7379be8fc25
Matt <mmunns16@gmail.com>
2019-04-22 15:32:03 -0500

    Modify size of the cards and add a second example card

--- public/wine.js
+++ public/wine.js
@@ -19,13 +19,14 @@ function WineCtrl($scope, wineApi) {
         },
         {
             taster: {
-                name: 'Roger Voss',
-                twitter: '@vossroger'
+                name: 'Michael Schachner',
+                twitter: '@wineschach'
             },
-            title: 'Demetria 2010 RosÃ© (Santa Ynez Valley)',
-            description: 'Bubblegummy and fresh in acidity, this rosÃ© tastes like a young Beaujolais, with strawberry and raspberry fruit. It\'s an enjoyable wine to wash down little tapas plates of olives, ham, salted nuts.',
-            points: 87,
-            price: 20
+            title: 'Segura Viudas NV Extra Dry Sparkling (Cava)',
+            description: '\"Baked plum, exotic spice and chocolate aromas almost jump out of the glass. The brawny palate doles out prune, blackberry jam, licorice and tobacco alongside round, velvety tannins. A raisin note b' +
+            'acks up the finish.\"',
+            points: 86,
+            price: 10
         }
     ]
 }
