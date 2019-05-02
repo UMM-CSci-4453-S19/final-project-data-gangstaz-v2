cbdc4b8567bd5a1cd014c7e89df4b273d9440858
CMenne <menne019@morris.umn.edu>
2019-05-02 15:32:14 -0500

    Remove placeholder data
    the getReviewArray() function sets the reviews array to the review objects returned from the server.

--- public/wine.js
+++ public/wine.js
@@ -25,36 +25,16 @@ app.config(function($routeProvider) {
 function WineCtrl($scope, wineApi) {
     // $scope stuff and functions go here
     $scope.getReviewArray = getReviewArray;
-    $scope.reviews = [
-        {
-            taster: {
-                name: 'Roger Voss',
-                twitter: '@vossroger'
-            },
-            title: 'Demetria 2010 RosÃ© (Santa Ynez Valley)',
-            description: 'Bubblegummy and fresh in acidity, this rosÃ© tastes like a young Beaujolais, with strawberry and raspberry fruit. It\'s an enjoyable wine to wash down little tapas plates of olives, ham, salted nuts.',
-            points: 87,
-            price: 20
+    $scope.reviews = [];
 
-        },
-        {
-            taster: {
-                name: 'Michael Schachner',
-                twitter: '@wineschach'
-            },
-            title: 'Segura Viudas NV Extra Dry Sparkling (Cava)',
-            description: '\"Baked plum, exotic spice and chocolate aromas almost jump out of the glass. The brawny palate doles out prune, blackberry jam, licorice and tobacco alongside round, velvety tannins. A raisin note b' +
-            'acks up the finish.\"',
-            points: 86,
-            price: 10
-        }
-    ];
-    
     function getReviewArray() {
-        wineApi.getReviews().then(function (success) {
-            console.log(success)
-            console.log("ran getReviewArray()")
-        })
+        wineApi.getReviews()
+            .then(function (success) {
+                $scope.reviews = success.data;
+            })
+            .catch(function (error) {
+                console.log(error);
+            })
     }
 
     getReviewArray();
