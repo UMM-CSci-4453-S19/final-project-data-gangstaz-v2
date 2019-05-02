3a5aab4235c92f88f86fdb0dd12bf354204165d9
nkfoss <nathankfoss@gmail.com>
2019-05-01 20:44:14 -0500

    Complete search query generator (text term AND dropboxes)

--- wine_search.js
+++ wine_search.js
@@ -1,55 +1,81 @@
 //rather than needing to stop and restart express all of the time, I am testing out the wine searching here
 
-//KEY:
-//vintage ->    Pre 2000's (pre2k), 2000-2009 (2ks), 2010's (210s)
+// Feel free to try this, by running node wine_search.js and uncomment the console.log
 
-var dropDownSQL = "select * from taster join wineReviews on tasterFk=tasterId join location on locFk=locId";
-var anyTermSQL = "OR";
+// (variety, vintage, continent, searchTerm)
+// console.log(buildSearchQuery(null, null, null, null))
 
-function anyTermSearch(searchTerm){
-    anyTermSQL = "OR";
+// vintage values:    Pre 2000's (pre2k), 2000-2009 (2ks), 2010's (2010s)
 
-    //   Fields per table that are not dropdowns:
-    //location: country, province, region, continent
-    //wineReviews: description, designation, points, price, winery
-    //taster: name, twitter
+var dropDownSQL = "";
+var anyTermSQL= "";
 
+function buildSearchQuery (variety, vintage, continent, searchTerm) {
 
-}
-
-function dropDownSearch(variety, vintage, continent) {
-
-    dropDownSQL = "select * from taster join wineReviews on tasterFk=tasterId join location on locFk=locId";
-
-    if(variety || vintage || continent){
-        dropDownSQL += " where ";
-
-        if(variety){
-            dropDownSQL += "variety = " + "'" + variety + "'" + " AND ";
-        }
-        if(vintage){
-            dropDownSQL += "vintage = " + "'" + vintage + "'" + " AND ";
-        }
-        if(continent){
-            dropDownSQL += "continent = " + "'" + continent + "'" + " AND ";
-        }
-        if (dropDownSQL.endsWith(" AND ")){
-            dropDownSQL = dropDownSQL.substring(0,dropDownSQL.length - 5);
-            dropDownSQL += ";";
-            console.log(dropDownSQL);
-        }
-
-
-    } else {
-        dropDownSQL += ";";
-        console.log(dropDownSQL);
+    // If search term is not provided, just make it blank.
+    if(!searchTerm) {
+        searchTerm = "";
     }
+
+    dropDownSQL = "select * from taster join wineReviews on tasterFk=tasterId join location on locFk=locId where ";
+    anyTermSQL = "(";
+
+    dropDownSearch(variety, vintage, continent, searchTerm);
+    anyTermSearch(searchTerm);
+
+    anyTermSQL += ");";
+
+    // Finally, return the final SQL search query.
+    return( dropDownSQL + anyTermSQL );
 }
 
-console.log(dropDownSearch("Red Blend",1996,"North America"));
-console.log(dropDownSearch("Blue Blend",1997,null));
-console.log(dropDownSearch("Green Blend",null,"South America"));
-console.log(dropDownSearch(null,1999,"Africa"));
+
+function dropDownSearch(variety, vintage, continent, searchTerm) {
+
+    if(vintage === "pre2k"){
+        dropDownSQL += "vintage < 2000 AND ";
+    } else if (vintage === "2ks") {
+        dropDownSQL += "vintage >= 2000 AND vintage < 2010 AND ";
+    } else if (vintage === "2010s") {
+        dropDownSQL += "vintage >= 2010 AND ";
+    }
+
+    if(variety){
+        dropDownSQL += "variety = " + "'" + variety + "'" + " AND ";
+        console.log("Adding to DDS....value = " + dropDownSQL)
+    } else {
+        anyTermSQL += "variety LIKE '%" + searchTerm + "%' OR ";
+        console.log("Adding to anyTerm....value = " + anyTermSQL)
+    }
+
+    if(continent){
+        dropDownSQL += "continent = " + "'" + continent + "'" + " AND ";
+        console.log("Adding to DDS....value = " + dropDownSQL)
+    } else {
+        anyTermSQL += "continent LIKE '%" + searchTerm + "%' OR ";
+        console.log("Adding to anyTerm....value = " + anyTermSQL)
+    }
+
+    console.log("dropDownSearch complete");
+    console.log("DDS = " + dropDownSQL);
+    console.log("anyTerm = " + anyTermSQL);
+}
+
+function anyTermSearch(searchTerm) {
+    anyTermSQL += "description LIKE '%" + searchTerm + "%' OR " +
+        "designation LIKE '%" + searchTerm + "%' OR " +
+        "winery LIKE '%" + searchTerm + "%' OR " +
+        "country LIKE '%" + searchTerm + "%' OR " +
+        "province LIKE '%" + searchTerm + "%' OR " +
+        "region LIKE '%" + searchTerm + "%' OR " +
+        "name LIKE '%" + searchTerm + "%' OR " +
+        "twitter LIKE '%" + searchTerm + "%' OR ";
+
+    if (anyTermSQL.endsWith(" OR ")) {
+        anyTermSQL = anyTermSQL.substring(0, anyTermSQL.length - 4);
+    }
+
+};
 
 
 
