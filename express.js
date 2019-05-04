9759af0ac13985c9fced8992fa29bc64dff6d7b9
michael-small <small203@morris.umn.edu>
2019-05-04 14:47:23 -0500

    Add various endpoints with incorrect SQL logic
    The SQL needs to be changed to a procedure that sorts fields correctly,
    but this is being pushed up so Chuck can use the rest of the code that
    was created for these changes in the overall wine categories.
    
    Adds highestRated, highestPrice, lowestPrice, bestValue
    
    #10

--- express.js
+++ express.js
@@ -72,7 +72,67 @@ app.get("/listAllOfType",function(req,res){
 
 app.get("/countries",function(req,res){
     var country = req.param('country');
-    var sql = 'Select description, designation, points, price, variety, winery, vintage from dataGangstas.wineReviews join dataGangstas.location on locFk = locId where country = ' + "\'" + country + "\'" + ';';
+    var sql = 'Select description, designation, points, price, variety, winery, vintage, province from dataGangstas.wineReviews join dataGangstas.location on locFk = locId where country = ' + "\'" + country + "\'" + ';';
+
+    connection.query(sql,(function(res){return function(err,rows,fields){
+        if(err) {
+            console.log(err);
+            res.send(err); // Let the upstream guy know how it went
+        }
+        else {
+            res.send(rows);
+        }
+    }})(res));
+});
+
+app.get("/highestRated",function(req,res){
+    var type = req.param('type');
+    var sql = 'Select country, description, variety, winery, vintage, province, max(points) as max from dataGangstas.wineReviews join dataGangstas.location on locFk = locId group by ' + type + ';';
+
+    connection.query(sql,(function(res){return function(err,rows,fields){
+        if(err) {
+            console.log(err);
+            res.send(err); // Let the upstream guy know how it went
+        }
+        else {
+            res.send(rows);
+        }
+    }})(res));
+});
+
+app.get("/highestPrice",function(req,res){
+    var type = req.param('type');
+    var sql = 'Select country, description, variety, winery, vintage, province, max(price) as maxPrice from dataGangstas.wineReviews join dataGangstas.location on locFk = locId group by ' + type + ';';
+
+    connection.query(sql,(function(res){return function(err,rows,fields){
+        if(err) {
+            console.log(err);
+            res.send(err); // Let the upstream guy know how it went
+        }
+        else {
+            res.send(rows);
+        }
+    }})(res));
+});
+
+app.get("/lowestPrice",function(req,res){
+    var type = req.param('type');
+    var sql = 'Select country, description, variety, winery, vintage, province, min(price) as minPrice from dataGangstas.wineReviews join dataGangstas.location on locFk = locId group by ' + type + ';';
+
+    connection.query(sql,(function(res){return function(err,rows,fields){
+        if(err) {
+            console.log(err);
+            res.send(err); // Let the upstream guy know how it went
+        }
+        else {
+            res.send(rows);
+        }
+    }})(res));
+});
+
+app.get("/bestValue",function(req,res){
+    var type = req.param('type');
+    var sql = 'Select country, description, variety, winery, vintage, province, points, price, max(points/price) as value from dataGangstas.wineReviews join dataGangstas.location on locFk = locId group by ' + type + ';';
 
     connection.query(sql,(function(res){return function(err,rows,fields){
         if(err) {
